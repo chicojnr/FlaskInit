778f2b6b5c486230c904cb8d61e33047dcc52f0c
chicojnr <souza.jnr@gmail.com>
2023-06-13 19:28:36 -0400

    update-add-pytube

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from pytube import YouTube
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    try:
+        video = YouTube('https://www.youtube.com/watch?v=' + request.args.get('id'))
+        title = video.title
+        thumb = video.thumbnail_url
+        publish_date = video.publish_date
+        views = video.views
+        channel_title = video.author
+        duration = video.length
+
+        data = {
+            "video": {
+                "title": title,
+                "publish_date": publish_date,
+                "views": views,
+                "channel_title": channel_title,
+                "thumb": thumb,
+                "duration": duration
+            }
+        }
+        return jsonify(data)
+    except Exception as e:
+        return json.dumps({"error": str(e)}, ensure_ascii=False)
 
 
 if __name__ == '__main__':
